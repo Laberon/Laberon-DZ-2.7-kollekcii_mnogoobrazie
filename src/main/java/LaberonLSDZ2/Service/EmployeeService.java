0fbcadee5fd3377dc0e90dfe6b57740c1524ac3b
Laberon <laberon83@gmail.com>
2022-08-26 16:35:35 +0300

    3

--- src/main/java/LaberonLSDZ2/Service/EmployeeService.java
+++ src/main/java/LaberonLSDZ2/Service/EmployeeService.java
@@ -11,30 +11,23 @@ import java.util.*;
 public class EmployeeService {
     private static final Map<String, Employee> employeeMap = new HashMap<>(5);
 
-    public static Employee addPerson(String firstName, String lastName, String middleName) throws EmployeeAlreadyAddedException {
+    public static Employee addPerson(String firstName, String lastName, String middleName) {
         Employee employee = new Employee(firstName, lastName,middleName);
         for (Employee employee1 : employeeMap.values()) {
-//            throw new EmployeeAlreadyAddedException("Такой сотрудник есть в базе");
         }
         return employeeMap.put(middleName, employee);
     }
 
-    public static Employee findPerson(String firstName, String lastName,String middleName)throws EmployeeNotFoundException {
+    public static Employee findPerson(String firstName, String lastName,String middleName){
         Employee employee = new Employee(firstName, lastName,middleName);
-        if (employee.getFirstName().equals(firstName) && Objects.equals(employee.getLastName(), lastName)) {
-            return employeeMap.get(middleName);
-        } else {
-            throw new EmployeeNotFoundException("Сотрудник не найден");
+        for (Employee employee1:employeeMap.values()) {
         }
+        return employeeMap.get(middleName);
     }
 
-    public static Employee removePerson(String middleName)throws EmployeeNotFoundException {
-        if (employeeMap.containsKey(middleName)) {
+    public static Employee removePerson(String middleName){
             return employeeMap.remove(middleName);
-        } else {
-            throw new EmployeeNotFoundException("Сотрудник не найден");
         }
-    }
 
     public static Collection<Employee> getAll() {
         return employeeMap.values();
